1b149fe05820e597d70b82f803c5b9adf8bc55dd
ehawk823 <evan.alexander.hawk@gmail.com>
2015-09-01 10:13:22 -0500

    done

--- db/migrate/20140310213347_create_posts.rb
+++ db/migrate/20140310213347_create_posts.rb
@@ -5,5 +5,7 @@ class CreatePosts < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+     add_column :posts, :content, :text
+     add_column :posts, :user_id, :integer, index: true
   end
 end
